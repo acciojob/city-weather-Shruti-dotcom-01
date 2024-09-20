f5d5c518ed486cc6a7e51bfd2e8e54a3552df396
shruti <shrutimsvaidya@gmail.com>
2024-09-20 17:42:48 +0530

    new

--- src/components/App.js
+++ src/components/App.js
@@ -1,75 +1,61 @@
 
-import React, { useEffect, useState } from "react";
-import './../styles/App.css';
-import 'core-js/stable';
-import 'regenerator-runtime/runtime';
+import "@babel/polyfill";
 
+import React,{useState,useEffect } from "react";
+import './../styles/App.css';
 
 const App = () => {
   const [city, setCity] = useState("");
-  const [apiData, setApiData] = useState(null);
-  const[apiCity, setApiCity] = useState("");
- 
+  const [search, setSearch] = useState("");
+  const [weather, setWeather] = useState({});
+  const [error, setError] = useState("");
+
   useEffect(() => {
-    const getData = async () => {
-      if (apiCity) {
-        try {
-          const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${apiCity}&appid=03848967c30bc88033d0f78a2df95495`);
-          const data = await res.json();
-           // Log data to see its structure
-          if (data.cod === 200) {
-            setApiData(data);
-            setApiCity(city)
-            setCity(""); 
-          } else {
-            setApiData(null);
-          }
-        } catch (error) {
-          console.error("Error fetching data:", error);
-          setApiData(null);
+  const fetchApi = async () => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=2d19741fec5e1ebbce4e4b02007b3734`;
+    fetch(url)
+      .then(response => {
+        if (!response.ok) {
+          setError('City not found');
+          throw new Error('City not found');
+          
         }
-      }
-    };
-    getData();
-  }, [apiCity]);
+        setError('')
+        return response.json();
+      })
+      .then(data => setWeather(data))
+      .catch(error => setWeather({error: error.message}));
+  }
+  fetchApi();
+}, [city]);
 
-  const toFahrenheit = (temp) => {
-    return parseInt((temp - 273.15) * 9 / 5 + 32);
-  };
-
+  
+  function display(){
+    setCity(search)
+  }
+   
   return (
-    <div className="App" style={{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", 
-      height: "100vh", width: "100vw", padding: "10px"}}>
-
- 
-    <div id="main" style={{ textAlign: "center", display: "flex", flexDirection: "column", alignItems: "center", 
-      height: "400px", width: "400px", backgroundColor: "lightgray", borderRadius: "10px", padding: "10px"
-     }}>
-      <input
-      className="search"
-      style={{ marginBottom: "10px" , padding: "10px", borderRadius: "10px", width: "300px", border: "none", outline: "none"}}
-        type="text"
-        placeholder="Enter city name"
-        value={city}
-        onChange={(e) => setCity(e.target.value)}
-        onKeyDown={(e) => e.key === "Enter" && setApiCity(city)}
-      />
-      {apiData && apiData.main ? (  
-        <div id="data" className="weather" >
-          <h1 id="city">{apiData.name}</h1>
-          <h2 id="temp">{toFahrenheit(apiData.main.temp)}°F</h2>
-          <p id="desc">{apiData.weather[0].description}</p>
-          <img
-            src={`https://openweathermap.org/img/wn/${apiData.weather[0].icon}@2x.png`}
-            alt="weather icon"
-          />
-        </div>
-      ) : (
-        apiCity && <p>No data available for the city!</p>
-      )}
+    <div className="main" id="main">
+      <div className="search">
+        <input type="text" placeholder="Enter a city" onChange={(e)=>setSearch(e.target.value)}/>
+        <button onClick={display}>Search</button>
+      </div>
+      <div className="weather">
+        { error && 
+          <h2>{error}</h2>
+        }
+        { weather.main && 
+          <>
+            <h1>{city}</h1>
+            <h2>{weather.main.temp}°Cel</h2>
+            <h3>{weather.weather[0].description}</h3>
+            <img src={`http://openweathermap.org/img/w/${weather.weather[0].icon}.png`} alt="cloud image"/>
+          </>
+        }
+      </div> 
     </div>
-    </div>
-  );
-};
+  )
+  
+}
 
-export default App;
+export default App
